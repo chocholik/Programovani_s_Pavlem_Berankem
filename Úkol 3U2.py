bcadc84fb952429e71b2b7745b591f28be449a5b
Jan Chochole <jan@chochole.com>
2021-05-24 18:12:22 +0200

    Přidání chytré verze

--- Úkol 3U2.py
+++ Úkol 3U2.py
@@ -1,14 +1,49 @@
 ###Úkol 3U2 - Hra šibenice
 ###Naprogramujte hru šibenice. Hráč hádá písmena v tajence a pokud písmeno neuhodne, ubírá se mu život. ###
 
-tajenka = "lokus"
+#tajenka = "kokos"
 nzivotu = 3
 skore = 0
+max_skore = 0
+tajenka_list = []
+tajenka_uhadnuto = []
+
+tajenka = input("Zadej co budeš hádat: ").strip().lower()
+
+for pismenko in tajenka: #naplnění listu písmeny
+    if pismenko.isalpha():
+        tajenka_list.append(pismenko)
+        tajenka_uhadnuto.append("_")
+    else:
+        tajenka_list.append(False)
+        tajenka_uhadnuto.append(pismenko)
+
+for i in tajenka_list: #Spočítám si max skore
+    if bool(i) == True:
+        max_skore += 1
+print(f"Hledáme tuto tajenku:\n{tajenka_uhadnuto}")
+
+while nzivotu != 0 and skore != max_skore: #budu prohledávat tajenku, co najdu, to odstraním z tajenky a ukážu to hráči
+    znak = input("Zadej písmenko z hádanky: ").strip( ).lower()
+    if znak in tajenka_list:
+        for pismenko in tajenka_list:
+            if znak == pismenko:
+                tajenka_uhadnuto[tajenka_list.index(znak)] = pismenko
+                tajenka_list[tajenka_list.index(znak)] = False
+                skore += 1
+    else:
+        nzivotu -= 1
+        print(f"Přišel si o život, zbývá {nzivotu}")
+    print(f"Stav:\nmáš uhádnuto {tajenka_uhadnuto}\nSkore je: {skore}\nZbává ti životů: {nzivotu}\n")
+if nzivotu == 0:
+  print(f"A je hotovo, přišel si o všechny životy. Dosažené skora je {skore}")
+else:
+  print(f"Gratuluji!!!!!\nVyhrál jsi!!!!\nZískal jsi body: {skore}\nZbývá životů: {nzivotu}")
 
 ''' HLOUPA VERZE
 while nzivotu != 0 and skore != len(tajenka):
 #  print(nzivotu != 0)
-#ll  print(skore != len(tajenka))
+#  print(skore != len(tajenka))
   znak = input("Zadej písmenko z hádanky: ")
   if znak in tajenka:
     skore += tajenka.count(znak)
@@ -21,3 +56,4 @@ if nzivotu == 0:
 else:
   print(f"Gratuluji, vyhrál jsi. Získal jsi {skore} bodů a ještě máš {nzivotu} život(y)")
 '''
+
